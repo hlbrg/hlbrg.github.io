18983c1fc0ad15fdf96b29e450b68d3ff9825d54
Danny Halberg <DannysMac@Dannys-MacBook-Air-4.local>
2014-10-20 16:41:57 +0200

    Bug fix - timer

---
+++ scoretracker/app.js
@@ -0,0 +1,13 @@
+var sec = 420;
+
+function pad(val) {
+    return val > 9 ? val : "0" + val;
+}
+var timer = setInterval(function () {
+    document.getElementById("seconds").innerHTML = pad(--sec % 60);
+    document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+}, 1000);
+
+setTimeout(function () {
+    clearInterval(timer);
+}, 420000);
