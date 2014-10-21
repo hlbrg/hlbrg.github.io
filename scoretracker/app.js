efda9a02250544eadaf4010361e9ebaa8242f02e
Danny Halberg <DannysMac@Dannys-MacBook-Air-4.local>
2014-10-21 09:55:35 +0200

    Reimplemented timer
    +some clean up

--- scoretracker/app.js
+++ scoretracker/app.js
@@ -1,13 +1,19 @@
-var sec = 420;
+//original time script - http://jsfiddle.net/fc37nckg/
+// var sec = 420;
 
-function pad(val) {
-    return val > 9 ? val : "0" + val;
-}
-var timer = setInterval(function () {
-    document.getElementById("seconds").innerHTML = pad(--sec % 60);
-    document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
-}, 1000);
+// function pad(val) {
+//     return val > 9 ? val : "0" + val;
+// }
+// var timer = setInterval(function () {
+//     document.getElementById("seconds").innerHTML = pad(--sec % 60);
+//     document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+// }, 1000);
 
-setTimeout(function () {
-    clearInterval(timer);
-}, 420000);
+// setTimeout(function () {
+//     clearInterval(timer);
+// }, 420000);
+
+// }
+
+
+
